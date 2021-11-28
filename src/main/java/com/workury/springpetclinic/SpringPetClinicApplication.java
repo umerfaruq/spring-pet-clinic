0b2780727195c9fe256489edd270bc3957e2f427
Umer Farooq <farooq.umer1999@gmail.com>
2021-11-28 17:56:09 +0500

    Setup Prettier and Pre-Commit Hook

--- src/main/java/com/workury/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/com/workury/springpetclinic/SpringPetClinicApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPetClinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPetClinicApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicApplication.class, args);
+    }
 }
