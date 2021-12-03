79a715ed4488482e648b8259d8132dbe04ccbf33
Umer Farooq <farooq.umer1999@gmail.com>
2021-12-03 21:39:05 +0000

    Add ID Property as Long Closes #12

--- pet-clinic-data/src/main/java/com/workury/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/workury/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.workury.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/workury/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/workury/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.workury.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
