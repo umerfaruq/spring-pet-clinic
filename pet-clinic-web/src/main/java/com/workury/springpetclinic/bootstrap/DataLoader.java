671a56f21ccca8a66c6af6ccb79c1ea0b135d780
Umer Farooq <farooq.umer1999@gmail.com>
2021-12-07 19:55:43 +0000

    Update Map Impl to manage setting ID Closes #26

--- pet-clinic-web/src/main/java/com/workury/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/workury/springpetclinic/bootstrap/DataLoader.java
@@ -26,28 +26,24 @@ public class DataLoader implements CommandLineRunner {
 
     private void loadData() {
         Owner owner = new Owner();
-        owner.setId(1L);
         owner.setFirstName("Michael");
         owner.setLastName("Weston");
 
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         Vet vet = new Vet();
-        vet.setId(1L);
         vet.setFirstName("Sam");
         vet.setLastName("Axe");
 
         vetService.save(vet);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
