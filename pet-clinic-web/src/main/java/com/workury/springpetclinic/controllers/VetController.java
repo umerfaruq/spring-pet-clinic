a89040ed519279aa7435208df35e19216095a535
Umer Farooq <farooq.umer1999@gmail.com>
2021-12-07 19:35:30 +0000

    List all Vets on Index Page Closes #18

--- pet-clinic-web/src/main/java/com/workury/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/workury/springpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.workury.springpetclinic.controllers;
 
+import com.workury.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping(value = "/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({ "/vets", "/vets/index", "/vets/index.html", "/vets.html" })
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({ "", "/", "/index", "/index.html" })
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
