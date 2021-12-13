9331ffb885b04ae60d4f54981e7094149dcbdb84
Umer Farooq <farooq.umer1999@gmail.com>
2021-12-13 18:49:44 +0000

    Create Vet Speciality Entity Closes #32

--- pet-clinic-data/src/main/java/com/workury/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/workury/springpetclinic/model/Vet.java
@@ -1,3 +1,16 @@
 package com.workury.springpetclinic.model;
 
-public class Vet extends Person {}
+import java.util.Set;
+
+public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+}
