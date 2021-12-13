40b0f15d20a0e92ee0a535a8a3e3a61e31eb059b
Umer Farooq <farooq.umer1999@gmail.com>
2021-12-13 19:04:20 +0000

    Create PetType Map Service Closes #34

---
+++ pet-clinic-data/src/main/java/com/workury/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,5 @@
+package com.workury.springpetclinic.services;
+
+import com.workury.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {}
---
+++ pet-clinic-data/src/main/java/com/workury/springpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.workury.springpetclinic.services.map;
+
+import com.workury.springpetclinic.model.PetType;
+import com.workury.springpetclinic.services.PetTypeService;
+import java.util.Set;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
