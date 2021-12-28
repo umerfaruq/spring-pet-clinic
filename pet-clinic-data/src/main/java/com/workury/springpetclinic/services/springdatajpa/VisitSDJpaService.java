95fde6cd1e702b8a2b6ed6e9fb5c76bd1938d765
Umer Farooq <farooq.umer1999@gmail.com>
2021-12-28 20:59:31 +0000

    Create SD JPA Service for Visit Closes #52

---
+++ pet-clinic-data/src/main/java/com/workury/springpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,44 @@
+package com.workury.springpetclinic.services.springdatajpa;
+
+import com.workury.springpetclinic.model.Visit;
+import com.workury.springpetclinic.repositories.VisitRepository;
+import com.workury.springpetclinic.services.VisitService;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return StreamSupport
+            .stream(visitRepository.findAll().spliterator(), false)
+            .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+}
